d6dd8038660576d40db5d352441f4c0fe0c80452
byNico1 <nicofoque0205@gmail.com>
2024-07-24 14:56:52 -0500

    add website config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = "Nicolas Forero Quevedo";
+export const SITE_DESCRIPTION =
+  "Eager and dynamic Apprentice from CodeCademy, CS50, Platzi and The Odin Project, I excel in software development, particularly with HTML, CSS, JS, Typescript, React, Node, Express and MongoDB, and demonstrate strong problem-solving abilities.";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;

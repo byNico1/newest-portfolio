ad6fdcf76b7a561dd64b66c76eb7cf2d3be97a53
byNico1 <nicofoque0205@gmail.com>
2024-07-24 16:52:16 -0500

    edit projects data

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -9,6 +9,7 @@ interface Project {
   githuburl?: string;
   projecturl?: string;
   badge?: string;
+  content: string[];
 }
 
 const projects: Project[] = [
@@ -16,51 +17,67 @@ const projects: Project[] = [
     _id: "promptopia",
     name: "Promptopia",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "A Next.js app leveraging MongoDB and Mongoose, allowing users to sign in and share their best ChatGPT prompts.",
     poster: "/promptopia.png",
-    slug: { _type: "slug", current: "#" },
-    date: "2023-01-01",
-    tags: ["Tag 1", "Tag 2"],
-    githuburl: "https://github.com/example/project1",
-    projecturl: "https://example.com/project1",
+    slug: { _type: "slug", current: "promptopia" },
+    date: "2024",
+    tags: ["Next.js", "MongoDb"],
+    githuburl: "https://github.com/byNico1/promptopia",
+    projecturl: "https://promptopia-nicolasforero.vercel.app/",
     badge: "NEW",
-  },
-  {
-    _id: "selfimprovingbooks",
-    name: "Selfimproving books",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    poster: "/selfimprovingbooks.png",
-    slug: { _type: "slug", current: "#" },
-    date: "2023-01-01",
-    tags: ["Tag 1", "Tag 2"],
-    githuburl: "https://github.com/example/project1",
-    projecturl: "https://example.com/project1",
+    content: [
+      "Technologies used: JavaScript, Next.js, MongoDB, Mongoose, Tailwind",
+      "Enabled users to Sign In, post, and categorize ChatGpt prompts like a social media",
+    ],
   },
   {
     _id: "cvapp",
     name: "CV App",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "A React.js and TypeScript-based web application that enables users to create and download resumes quickly and effortlessly.",
     poster: "/cv_app.png",
-    slug: { _type: "slug", current: "#" },
-    date: "2023-01-01",
-    tags: ["Tag 1", "Tag 2"],
-    githuburl: "https://github.com/example/project1",
-    projecturl: "https://example.com/project1",
+    slug: { _type: "slug", current: "cv-app" },
+    date: "2024-02-15",
+    tags: ["TypeScript", "React"],
+    githuburl: "https://github.com/byNico1/react-cv-app",
+    projecturl: "https://nicoiscoding-react-cv-app.netlify.app/",
     badge: "NEW",
+    content: [
+      "Technologies used: Typescript, React.js, Tailwind",
+      "Helps you generate and download your resume fast",
+    ],
+  },
+  {
+    _id: "selfimprovingbooks",
+    name: "Selfimproving books",
+    description:
+      "An Astro.js website designed for showcasing Amazon Affiliate products with a strong emphasis on SEO, utilizing React and TypeScript.",
+    poster: "/selfimprovingbooks.png",
+    slug: { _type: "slug", current: "selfimprovingbooks" },
+    date: "2024-02-01",
+    tags: ["Astro", "TypeScript"],
+    githuburl: "https://github.com/byNico1/selfimprovingbooks",
+    projecturl: "https://selfimprovingbooks.com",
+    content: [
+      "Technologies used: Astro, JavaScript, Mdx, Typescript",
+      "SEO-optimized website with AI to reach more people and make revenue with the Amazon Affiliate Program",
+    ],
   },
   {
     _id: "battleship",
     name: "Battleship",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "A React.js-powered website recreating the classic Battleship game, providing an interactive and engaging user experience.",
     poster: "/battleship.png",
-    slug: { _type: "slug", current: "#" },
-    date: "2023-01-01",
-    tags: ["Tag 1", "Tag 2"],
-    githuburl: "https://github.com/example/project1",
-    projecturl: "https://example.com/project1",
+    slug: { _type: "slug", current: "battleship" },
+    date: "2024-02-03",
+    tags: ["React", "Tailwind"],
+    githuburl: "https://github.com/byNico1/battleship",
+    projecturl: "https://bynico1.github.io/battleship/",
+    content: [
+      "Technologies used: React, JavaScript, Tailwind",
+      "Recreated battleship game with React.js",
+    ],
   },
 ];
 
